2350288b4edc0cd65e93bd9b5371538a750943f8
PainDe0Mie <97704518+PainDe0Mie@users.noreply.github.com>
2023-08-15 00:01:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,40 +37,76 @@ client.config = config;
 
 client.login(config.bottoken);
 
+client.on('ready', async () => {
+    console.log(`${client.user.tag} est bien lancé !`);
 
+    const sites = [
+        {
+            addr: config.ipweb1,
+            port: config.portweb,
+            nameweb: config.nameweb1,
+        },
+        {
+            addr: config.ipweb2,
+            port: config.portweb,
+            nameweb: config.nameweb2,
+        },
+        {
+            addr: config.ipweb3,
+            port: config.portweb,
+            nameweb: config.nameweb2,
+        },
+        {
+            addr: "cloudflare.com",
+            port: config.portweb,
+            nameweb: "Proxy cloudflare",
+        },
+    ];
 
-client.on('ready', () => {
-    console.log('ready')
-    let addr = config.ipweb0,
-    port = config.portweb0;
-    nameweb0 = config.nameweb0;
-    
-    var currentDate = new Date();
-    let ping = new EmbedBuilder()
-    .setTitle(`Verification`)
-    .setColor('Blue')
-    .addFields({ name: `Récupération des différents Variable `, value: 'chargement...'})
-    .setFooter({ text: `Dernière actualisation le ${new Date().toLocaleString('fr-FR',{timeZone: "Europe/Paris"})}` })
-    client.channels.resolve(config.setchannel).send({ embeds: [ping] })
-    .then(msg => {
+    try {
+        const channel = client.channels.resolve(config.setchannel);
+        let user = client.user.id
+        let number = 100;
+        let messages = (await channel.messages.fetch({ limit: number })).filter(m => m.author.id === user);
+        if (messages.length <= 0) return;
+        await channel.bulkDelete(messages);
+         const start = new EmbedBuilder()
+         .setColor("Red")
+         .setDescription("*Chargement en cours...*")
+         const msg = await channel.send({ embeds: [start] });
+         var online = '🟢'
+         var offline = '🔴'
+
         setInterval(() => {
-            tcpp.probe(addr, port, function(err, available0) {
-                    tcpp.ping({ address: addr, port: port }, function(err, data) {
-                        var online = '🟢'
-                        var offline = '🔴'
-                        if (available0 == true) {var web = `${online} | [${nameweb0}](https://${addr}) (${Math.floor(data.avg)}ms)`} else {var web = `${offline} | [${nameweb0}](https://${addr}) `} 
-                        
-                        let ping = new EmbedBuilder()
-                        .setTitle("Statut Infrastructure :")
-                        .setColor('Blue')
-                        .addFields({ name: '_ _', value: '_ _', inline: true }) 
-                        .addFields({ name: `☁️** — Nom du web:** `, value: `${web}` })
-                        .addFields({ name: '_ _', value: '_ _', inline: true })
-                        .addFields({ name: `» Légende :`, value: `${online} = Service opérationnel \n${offline} = Service hors-ligne \n` })
-                        .setFooter({ text: `Dernière actualisation : ${new Date().toLocaleString('fr-FR',{timeZone: "Europe/Paris"})}`})
-                        msg.edit({ embeds: [ping] })
+            const pingFields = sites.map(site => {
+                return new Promise((resolve) => {
+                    tcpp.probe(site.addr, site.port, function (err, available) {
+                        tcpp.ping({ address: site.addr, port: site.port }, function (err, data) {
+                            var web = available ? `${online} | [${site.nameweb}](https://${site.addr}) (${Math.floor(data.avg)}ms)` : `${offline} | [${site.nameweb}](https://${site.addr})`;
+
+                            resolve({
+                                name: `☁️ - **${site.nameweb}**`,
+                                value: web
+                            });
+                        });
+                    });
                 });
-        })
-        }, 300) 
-    })
-})
+            });
+
+            Promise.all(pingFields).then(fields => {
+                const pingEmbed = new EmbedBuilder()
+                    .setTitle(":chart_with_upwards_trend: • Statut des services :")
+                	.setURL("https://stats.uptimerobot.com/DOlqPSjoz6")
+                    .setColor('#4285F4')
+                    .addFields(...fields)
+                    .addFields({ name: '_ _', value: '_ _', inline: true })
+                    .addFields({ name: `» Légende :`, value: `${online} = Service opérationnel \n${offline} = Service hors-ligne \n` })
+                    .setFooter({ text: `Dernière actualisation : ${new Date().toLocaleString('fr-FR', { timeZone: "Europe/Paris" })}` });
+
+                msg.edit({ embeds: [pingEmbed] });
+            });
+        }, 10000);
+    } catch (erreur) {
+        console.error('Erreur :', erreur);
+    }
+});
